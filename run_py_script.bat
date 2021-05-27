ab617bb62fd746c719e1ae6adf4e3b2b46831604
SamFreitas1996 <samfreitas@email.arizona.edu>
2021-05-27 11:49:28 -0700

    update 2

---
+++ run_py_script.bat
@@ -0,0 +1,2 @@
+python "/Users/samfreitas/Documents/python/WW_backup/scan_folders_for_png.py"
+pause
--- scan_folders_for_png.py
+++ scan_folders_for_png.py
@@ -6,12 +6,18 @@ from natsort import natsorted
 import shutil
 # import tkinter as tk
 
+
+# path to the main working WormWatcher folder
 filesPath = '/Volumes/WormWatcher/WormWatcher'
 
+# number of days want to store as a backup
+# default 12
 days_of_backup = 12
 
+# path to the backup folder, probably on a seperate hard drive
 backup_path = '/e/WW_backup/WormWatcher'
 
+# list of required dirs 
 list_of_required_dirs = [
 'backups','CoolTerm Libs','CoolTerm Resources','Dependents','Documents',
 'logs','models','old_params_05-Feb-2019_10-28-50','old_params_06-Mar-2019_15-23-58',
@@ -20,65 +26,67 @@ list_of_required_dirs = [
 'WWAnalyzer','WWAnalyzer 2019-06-04 BETA','WWConfig','WWConfigPy','WWCore','WWPythonSupplements','Samples',
 ]
 
+# find X days before current time
 criticalTime = arrow.now().shift(days=-days_of_backup)
 
-# all_files = glob.glob(os.path.join(filesPath), recursive=True)
-
-# for item in Path(filesPath).glob('/*'):
-
+# get all the directories from the working directory
 dir_list = natsorted(glob.glob(os.path.join(filesPath,'*/')))
 
+# iterate through each dir in the glob list
 for item in dir_list:
 
+    # find the basename
     item_name = os.path.basename(os.path.dirname(item))
 
+    # if the basename is NOT included in the required dirs
     if item_name not in list_of_required_dirs:
+        print('')
         print(item_name)
 
+        # get the pathlib.Path of this dir
         item_path = Path(item)
 
+        # recursively go through item_path and list all .pngs (Can be changed to whatever storage medium)
         list_of_png_paths = list(item_path.glob('**/*.png'))
 
+        # number of items moved to backup
+        backup_counter = 0
+
+        # if not empty
         if list_of_png_paths:
             
-            for count,this_path in enumerate(list_of_png_paths):
+            # iterate through each png
+            for _,this_path in enumerate(list_of_png_paths):
+
+                # find how old the file is
                 itemTime = arrow.get(this_path.stat().st_mtime)
 
+                # if the file is older than X days
                 if itemTime < criticalTime:
+                    
+                    # convert to a string
                     this_path_str = str(this_path)
+                    # get where the file is going
                     move_to = this_path_str.replace(filesPath,backup_path)
-
+                    # find head and tail (path and name) of where file is going
                     head,tail = os.path.split(move_to)
 
+                    # if path already to where its going already exists 
                     if os.path.isdir(head):
-                        pass
+                        # move file from this_path_str to move_to 
+                        # shutil.move(this_path_str,move_to)
+                        backup_counter = backup_counter + 1 
                     else:
-                        os.makedirs(head)
-
-                    pass
+                        # recursively make the dirs to where its going
+                        # os.makedirs(head)
+                        # move file from this_path_str to move_to 
+                        # shutil.move(this_path_str,move_to)
+                        backup_counter = backup_counter + 1
                 else:
-                    print(this_path)
+                    # item is not old enough to be moved to backup
+                    pass
 
         print(len(list_of_png_paths), 'items in',item_path)
+        print(backup_counter, 'items moved')
 
-        # for path in Path(item).rglob('*.png'):
-
-        #     list_of_png_paths.append(path._str)
-
-
-
-# ww_path =
-
-# all_folders = glob.glob(os.path.join(ww_path,'*/'))
-
-# list_of_png_paths = []
-
-# if os.path.isdir(ww_path):
-
-#     for path in Path(ww_path).rglob('*.png'):
-#         list_of_png_paths.append(path._str)
-
-# else:
-#     print('Not connectect to server')
-
-# print('end')
+print('end of script')
